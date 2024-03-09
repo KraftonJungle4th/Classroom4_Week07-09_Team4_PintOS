a5476056cffc551eb3efc4a1baa2ed4bdf4938a7
jeemh <jimyeongheon@gmail.com>
2024-03-09 19:05:04 +0900

    feat:

--- threads/synch.c
+++ threads/synch.c
@@ -202,7 +202,9 @@ lock_acquire (struct lock *lock) {
 	ASSERT (!intr_context ());
 	ASSERT (!lock_held_by_current_thread (lock));
 
-	lock->holder = thread_current();
+	if(lock->holder)
+		lock->holder = thread_current();
+	
 
 	sema_down (&lock->semaphore);
 	lock->holder = thread_current ();
